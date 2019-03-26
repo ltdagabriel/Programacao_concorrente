7de4b47ece84e615428ec7a114486489191558eb
ltdagabriel <choptian@alunos.utfpr.edu.br>
2019-03-26 19:54:01 -0300

    Exercicio 3

--- src/lista1/Implement_threads.java
+++ src/lista1/Implement_threads.java
@@ -13,7 +13,12 @@ public class Implement_threads extends Thread {
     public void run() {
         try {
             sleep(1);
-            System.out.println("-: " + cnt.plus());
+            if(cnt == null){
+                System.out.println("Hello Thread");
+            }
+            else {
+                System.out.println(cnt.plus());
+            }
 
         } catch (InterruptedException e) {
             e.printStackTrace();
--- src/lista1/exercicio_3.java
+++ src/lista1/exercicio_3.java
@@ -1,11 +1,18 @@
 package lista1;
 
+import java.util.concurrent.TimeUnit;
+
 public class exercicio_3 {
-    public static void main(String[] args){
+    public static void main(String[] args) throws InterruptedException {
         Contador temp = new Contador();
-        for(int i=0; i< 10000;i++){
+        int n = 10000;
+        for(int i=0; i< n;i++){
             Implement_threads a = new Implement_threads(temp);
             a.start();
+        }
+        TimeUnit.SECONDS.sleep(1);
+        if (temp.getNumber() < n){
+            System.out.println("Falha de seguranção contador menor que o numero de threads!");
         }
     }
 }
